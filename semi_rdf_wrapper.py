c893835a4bf9f2293b26693816c0586b24002342
Jakob Nyberg <jaknyb@kth.se>
2024-10-07 10:35:29 +0200

    move code into utils

--- semi_rdf_wrapper.py
+++ semi_rdf_wrapper.py
@@ -1,10 +1,11 @@
 from copy import copy
 import random
-from typing import Any, TypeVar, NamedTuple
+from typing import Any, NamedTuple
 import pyRDDLGym
 import numpy as np
 import gymnasium as gym
 from enum import Enum
+from utils import generate_bipartite_obs
 
 
 class Arity(Enum):
@@ -43,94 +44,8 @@ def to_graphviz(
     return graph
 
 
-def predicate(key: str) -> str:
-    return key.split("___")[0]
-
-
-def objects(key: str) -> list[str]:
-    split = key.split("___")
-    return split[1].split("__") if len(split) > 1 else []
-
-
-def translate_edges(
-    source_symbols: list[str], target_symbols: list[str], edges: set[Edge]
-):
-    return np.array(
-        [(source_symbols.index(key[0]), target_symbols.index(key[1])) for key in edges],
-        dtype=np.uint,
-    )
-
-
-T = TypeVar("T")
-
-
-def edge_attr(edges: set[Edge]) -> np.ndarray[np.uint, Any]:
-    return np.array([key[2] for key in edges], dtype=np.uint)
-
-
-def create_edges(d: dict[str, Any]) -> set[Edge]:
-    edges: set[Edge] = set()
-    for key in d:
-        for pos, object in enumerate(objects(key)):
-            new_key = Edge(key, object, pos)
-            edges.add(new_key)
-    return edges
-
-
 def is_fluent_to_skip(key: str, obs: dict[str, bool]) -> bool:
     return isinstance(obs[key], np.bool_) and not obs[key]
-
-
-def generate_bipartite_obs(
-    obs: dict[str, bool],
-    groundings: list[str],
-    symb_to_idx: dict[str, int],
-    variable_ranges: dict[str, str],
-) -> tuple[
-    np.ndarray[np.bool_, Any],
-    np.ndarray[np.int_, Any],
-    np.ndarray[np.uint, Any],
-    np.ndarray[np.uint, Any],
-    np.ndarray[np.bool_, Any],
-]:
-    edges: set[Edge] = create_edges(obs)
-    obs_objects: set[str] = set()
-
-    for key in obs:
-        for object in objects(key):
-            obs_objects.add(object)
-
-    object_list: list[str] = sorted(obs_objects)
-
-    object_nodes = np.array(
-        [symb_to_idx[object] for object in object_list], dtype=np.int32
-    )
-    fact_node_values = np.array([obs[key] for key in groundings], dtype=np.float32)
-
-    fact_node_predicate = np.array(
-        [symb_to_idx[predicate(key)] for key in groundings], dtype=np.int_
-    )
-
-    edge_indices = translate_edges(groundings, object_list, edges)
-
-    edge_attributes = edge_attr(edges)
-
-    fact_nodes = np.stack(
-        [fact_node_values, fact_node_predicate], axis=1, dtype=np.float_
-    )
-
-    assert max(edge_indices[:, 0]) < len(fact_nodes)
-    assert max(edge_indices[:, 1]) < len(object_nodes)
-
-    numeric = np.array(
-        [
-            1 if variable_ranges[predicate(g)] in ["real", "int"] else 0
-            for g in groundings
-        ],
-        dtype=np.bool_,
-    )
-
-    return (fact_nodes, object_nodes, edge_indices, edge_attributes, numeric)
 
 
 class RDDLGraphWrapper(gym.Wrapper):
@@ -173,13 +88,10 @@ class RDDLGraphWrapper(gym.Wrapper):
         non_fluents: list[str] = list(env.model.non_fluents.keys())  # type: ignore
 
         action_values = {k: np.bool_(True) for k in action_groundings}
-        action_edges: set[Edge] = create_edges(action_values)
 
         non_fluent_values: dict[str, int] = dict(
             env.model.ground_vars_with_values(model.non_fluents)  # type: ignore
         )
-
-        non_fluent_edges: set[Edge] = create_edges(non_fluent_values)
 
         object_terms: list[str] = list(model.object_to_index.keys())  # type: ignore
         action_fluents: list[str] = list(model.action_fluents.keys())  # type: ignore
@@ -191,8 +103,6 @@ class RDDLGraphWrapper(gym.Wrapper):
         self.variable_ranges = variable_ranges  # type: ignore
         symb_to_idx = {symb: idx for idx, symb in enumerate(symbol_list)}
         self.non_fluents_values = non_fluent_values
-        self.non_fluent_edges = non_fluent_edges
-        self.action_edges = action_edges
         self.type_to_fluent = type_to_fluent
         self.model = model
         self.num_objects = num_objects

49be4e78101faad3c0b6fd71d6c42203c2b27adb
Jakob Nyberg <jaknyb@kth.se>
2024-10-10 11:13:14 +0200

    utility funcs

---
+++ parent_wrapper.py
@@ -0,0 +1,163 @@
+from copy import copy
+import random
+import pyRDDLGym
+import numpy as np
+import gymnasium as gym
+from utils import to_graphviz
+
+
+class RDDLGraphWrapper(gym.Wrapper):
+    metadata = {"render.modes": ["human"]}
+
+    def __init__(self, domain: str, instance: int, render_mode: str = "human") -> None:
+        env = pyRDDLGym.make(domain, instance, enforce_action_constraints=True)  # type: ignore
+        model = env.model  # type: ignore
+        object_to_type: dict[str, str] = copy(model.object_to_type)  # type: ignore
+        types = set(object_to_type.values())  # type: ignore
+        num_objects = sum(env.model.object_counts(types))  # type: ignore
+        state_fluents: list[str] = list(env.model.state_fluents.keys())  # type: ignore
+        action_groundings: list[str] = list(
+            dict(env.model.ground_vars_with_values(model.action_fluents)).keys()  # type: ignore
+        )
+        groundings: list[str] = [
+            g
+            for _, v in env.model.variable_groundings.items()  # type: ignore
+            for g in v  # type: ignore
+            if g[-1] != env.model.NEXT_STATE_SYM  # type: ignore
+        ]
+
+        groundings = sorted(set(groundings))
+        variable_params: dict[str, list[str]] = copy(env.model.variable_params)  # type: ignore
+        type_to_fluent: dict[str, list[str]] = {
+            value[0]: [k for k, v in variable_params.items() if v == value]
+            for _, value in variable_params.items()
+            if len(value) == 1
+        }
+        arities: dict[str, int] = {
+            key: len(value) for key, value in variable_params.items()
+        }
+        arities_to_fluent: dict[int, list[str]] = {
+            value: [k for k, v in arities.items() if v == value]
+            for _, value in arities.items()
+        }
+        assert max(arities.values()) <= 2, "Only up to binary predicates are supported"
+        non_fluents: list[str] = list(env.model.non_fluents.keys())  # type: ignore
+
+        action_values = {k: np.bool_(True) for k in action_groundings}
+
+        non_fluent_values: dict[str, int] = dict(
+            env.model.ground_vars_with_values(model.non_fluents)  # type: ignore
+        )
+
+        object_terms: list[str] = list(model.object_to_index.keys())  # type: ignore
+        action_fluents: list[str] = list(model.action_fluents.keys())  # type: ignore
+        symbol_list = sorted(
+            object_terms + non_fluents + state_fluents + action_fluents
+        )
+        variable_ranges: dict[str, str] = model._variable_ranges  # type: ignore
+
+        self.variable_ranges = variable_ranges  # type: ignore
+        symb_to_idx = {symb: idx for idx, symb in enumerate(symbol_list)}
+        self.non_fluents_values = non_fluent_values
+
+        self.instance = instance
+        self.domain = domain
+        self.type_to_fluent = type_to_fluent
+        self.model = model
+        self.num_objects = num_objects
+        self.arities = arities
+        self.arities_to_fluent = arities_to_fluent
+        self.non_fluents = non_fluents
+        self.action_values = action_values
+        self.object_to_type = object_to_type
+        self.groundings = groundings
+        self.state_fluents = state_fluents
+        self.idx_to_symb = symbol_list
+        self.symb_to_idx = symb_to_idx
+        self.env = env
+        self.iter = 0
+        self.observation_space = gym.spaces.Dict(
+            {
+                "nodes": gym.spaces.Box(
+                    low=0, high=1, shape=(len(groundings), 2), dtype=np.float32
+                ),
+                "object_nodes": gym.spaces.Box(
+                    low=0, high=len(symbol_list), shape=(num_objects,), dtype=np.int32
+                ),
+                "edge_indices": gym.spaces.Box(
+                    low=0,
+                    high=num_objects,
+                    shape=(len(action_groundings) + len(non_fluent_values), 2),
+                    dtype=np.uint,
+                ),
+                "edge_attributes": gym.spaces.Box(
+                    low=0,
+                    high=1,
+                    shape=(len(action_groundings) + len(non_fluent_values),),
+                    dtype=np.uint,
+                ),
+                "numeric": gym.spaces.Box(
+                    low=0,
+                    high=1,
+                    shape=(len(symbol_list),),
+                    dtype=np.bool_,  # type: ignore
+                ),
+            }
+        )
+        # self.action_space = gym.spaces.Discrete(len(groundings))
+
+    def render(self):
+        obs = self.last_obs
+        nodes_classes = obs["predicate_class"]
+        node_values = obs["predicate_value"]
+        object_nodes = obs["object"]
+        edge_indices = obs["edge_index"]
+        edge_attributes = obs["edge_attr"]
+        numeric = obs["numeric"]
+
+        with open(f"{self.domain}_{self.instance}_{self.iter}.dot", "w") as f:
+            f.write(
+                to_graphviz(
+                    nodes_classes,
+                    node_values,
+                    object_nodes,
+                    edge_indices,
+                    edge_attributes,
+                    self.idx_to_symb,
+                    numeric,
+                )
+            )
+
+
+def main():
+    instance = 1
+    domain = "Elevators_MDP_ippc2011"
+    # domain = "SysAdmin_MDP_ippc2011"
+    # domain = "RecSim_ippc2023"
+    # domain = "SkillTeaching_MDP_ippc2011"
+    env = RDDLGraphWrapper(domain, instance)
+    obs, info = env.reset()
+    env.render()
+    done = False
+    time = 0
+    sum_reward = 0
+    while not done:
+        time += 1
+        action = env.env.action_space.sample()
+
+        action = random.choice(list(action.items()))
+        action = {action[0]: action[1]}
+        obs, reward, terminated, truncated, info = env.step(action)
+        env.render()
+        exit()
+        done = terminated or truncated
+
+        sum_reward += reward
+        # print(obs)
+        print(action)
+        print(reward)
+    print(sum_reward)
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,11 +1,20 @@
 from typing import NamedTuple, TypeVar, Any
 import numpy as np
-
+import random
+from gymnasium import spaces
 
 T = TypeVar("T")
 
 
 Edge = NamedTuple("Edge", [("predicate", str), ("object", str), ("pos", int)])
+
+
+def sample_action(action_space) -> dict[str, int]:
+    action: dict[str, int] = action_space.sample()
+
+    action: tuple[str, int] = random.choice(list(action.items()))
+    action = {action[0]: action[1]}
+    return action
 
 
 def predicate(key: str) -> str:
@@ -45,6 +54,7 @@ def generate_bipartite_obs(
     symb_to_idx: dict[str, int],
     variable_ranges: dict[str, str],
 ) -> tuple[
+    np.ndarray[np.int_, Any],
     np.ndarray[np.bool_, Any],
     np.ndarray[np.int_, Any],
     np.ndarray[np.uint, Any],
@@ -73,11 +83,8 @@ def generate_bipartite_obs(
 
     edge_attributes = edge_attr(edges)
 
-    fact_nodes = np.stack(
-        [fact_node_values, fact_node_predicate], axis=1, dtype=np.float_
-    )
-
-    assert max(edge_indices[:, 0]) < len(fact_nodes)
+    assert max(edge_indices[:, 0]) < len(fact_node_values)
+    assert max(edge_indices[:, 0]) < len(fact_node_predicate)
     assert max(edge_indices[:, 1]) < len(object_nodes)
 
     numeric = np.array(
@@ -88,4 +95,44 @@ def generate_bipartite_obs(
         dtype=np.bool_,
     )
 
-    return (fact_nodes, object_nodes, edge_indices, edge_attributes, numeric)
+    return (
+        fact_node_predicate,
+        fact_node_values,
+        object_nodes,
+        edge_indices,
+        edge_attributes,
+        numeric,
+    )
+
+
+def to_graphviz(
+    first_node_classes,
+    first_node_values,
+    second_nodes,
+    edges,
+    edge_attributes,
+    idx_to_symb,
+    numeric,
+):
+    colors = ["red", "green", "blue", "yellow", "purple", "orange", "cyan", "magenta"]
+    graph = "graph G {\n"
+    first_mapping = {}
+    second_mapping = {}
+    global_idx = 0
+    for idx, n_class in enumerate(first_node_classes):
+        label = (
+            f'"{idx_to_symb[int(n_class)]}={first_node_values[idx]}"'
+            if numeric[idx]
+            else f'"{idx_to_symb[int(n_class)]}={bool(first_node_values[idx])}"'
+        )
+        graph += f'"{global_idx}" [label={label}, shape=box]\n'
+        first_mapping[idx] = global_idx
+        global_idx += 1
+    for idx, data in enumerate(second_nodes):
+        graph += f'"{global_idx}" [label="{idx_to_symb[data]}", shape=circle]\n'
+        second_mapping[idx] = global_idx
+        global_idx += 1
+    for attribute, edge in zip(edge_attributes, edges):
+        graph += f'"{first_mapping[edge[0]]}" -- "{second_mapping[edge[1]]}" [color="{colors[attribute]}"]\n'
+    graph += "}"
+    return graph

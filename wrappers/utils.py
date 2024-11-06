dcd766508bbd677e3ffe66d6620e72e9a8d36925
Jakob Nyberg <jaknyb@kth.se>
2024-11-06 12:59:56 +0100

    utils

--- wrappers/utils.py
+++ wrappers/utils.py
@@ -31,12 +31,12 @@ def translate_edges(
 ):
     return np.array(
         [(source_symbols.index(key[0]), target_symbols.index(key[1])) for key in edges],
-        dtype=np.uint,
+        dtype=np.int64,
     )
 
 
 def edge_attr(edges: set[Edge]) -> np.ndarray[np.uint, Any]:
-    return np.array([key[2] for key in edges], dtype=np.uint)
+    return np.array([key[2] for key in edges], dtype=np.int64)
 
 
 def create_edges(d: dict[str, Any]) -> set[Edge]:
@@ -51,7 +51,9 @@ def create_edges(d: dict[str, Any]) -> set[Edge]:
 def generate_bipartite_obs(
     obs: dict[str, bool],
     groundings: list[str],
-    symb_to_idx: dict[str, int],
+    rel_to_idx: dict[str, int],
+    type_to_idx: dict[str, int],
+    obj_to_type: dict[str, str],
     variable_ranges: dict[str, str],
 ) -> tuple[
     np.ndarray[np.int_, Any],
@@ -71,12 +73,13 @@ def generate_bipartite_obs(
     object_list: list[str] = sorted(obs_objects)
 
     object_nodes = np.array(
-        [symb_to_idx[object] for object in object_list], dtype=np.int32
+        [type_to_idx[obj_to_type[object]] for object in object_list], dtype=np.int64
     )
-    fact_node_values = np.array([obs[key] for key in groundings], dtype=np.float32)
+
+    fact_node_values = np.array([obs[key] for key in groundings], dtype=np.bool_)
 
     fact_node_predicate = np.array(
-        [symb_to_idx[predicate(key)] for key in groundings], dtype=np.int_
+        [rel_to_idx[predicate(key)] for key in groundings], dtype=np.int64
     )
 
     edge_indices = translate_edges(groundings, object_list, edges)
@@ -106,12 +109,13 @@ def generate_bipartite_obs(
 
 
 def to_graphviz(
-    first_node_classes,
-    first_node_values,
-    second_nodes,
+    predicate_node_classes,
+    predicate_node_values,
+    object_nodes,
     edges,
     edge_attributes,
-    idx_to_symb,
+    obj_to_idx,
+    rel_to_idx,
     numeric,
 ):
     colors = ["red", "green", "blue", "yellow", "purple", "orange", "cyan", "magenta"]
@@ -119,17 +123,46 @@ def to_graphviz(
     first_mapping = {}
     second_mapping = {}
     global_idx = 0
-    for idx, n_class in enumerate(first_node_classes):
+    for idx, n_class in enumerate(predicate_node_classes):
         label = (
-            f'"{idx_to_symb[int(n_class)]}={first_node_values[idx]}"'
+            f'"{rel_to_idx[int(n_class)]}={predicate_node_values[idx]}"'
             if numeric[idx]
-            else f'"{idx_to_symb[int(n_class)]}={bool(first_node_values[idx])}"'
+            else f'"{rel_to_idx[int(n_class)]}={bool(predicate_node_values[idx])}"'
         )
         graph += f'"{global_idx}" [label={label}, shape=box]\n'
         first_mapping[idx] = global_idx
         global_idx += 1
-    for idx, data in enumerate(second_nodes):
-        graph += f'"{global_idx}" [label="{idx_to_symb[data]}", shape=circle]\n'
+    for idx, data in enumerate(object_nodes):
+        graph += f'"{global_idx}" [label="{obj_to_idx[data]}", shape=circle]\n'
+        second_mapping[idx] = global_idx
+        global_idx += 1
+    for attribute, edge in zip(edge_attributes, edges):
+        graph += f'"{first_mapping[edge[0]]}" -- "{second_mapping[edge[1]]}" [color="{colors[attribute]}"]\n'
+    graph += "}"
+    return graph
+
+
+def to_graphviz_alt(
+    predicate_node_classes: list[int],
+    predicate_node_values: list[bool],
+    object_nodes: list[int],
+    edges: list[tuple[int, int]],
+    edge_attributes: list[int],
+    idx_to_type: dict[int, str],
+    idx_to_rel: dict[int, str],
+):
+    colors = ["red", "green", "blue", "yellow", "purple", "orange", "cyan", "magenta"]
+    graph = "graph G {\n"
+    first_mapping = {}
+    second_mapping = {}
+    global_idx = 0
+    for idx, n_class in enumerate(predicate_node_classes):
+        label = f'"{idx_to_rel[int(n_class)]}={bool(predicate_node_values[idx])}"'
+        graph += f'"{global_idx}" [label={label}, shape=box]\n'
+        first_mapping[idx] = global_idx
+        global_idx += 1
+    for idx, data in enumerate(object_nodes):
+        graph += f'"{global_idx}" [label="{idx_to_type[data]}", shape=circle]\n'
         second_mapping[idx] = global_idx
         global_idx += 1
     for attribute, edge in zip(edge_attributes, edges):

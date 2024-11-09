42f3dcaeedb1366dac869bdb52fc12fc51812c14
Jakob Nyberg <jaknyb@kth.se>
2024-11-09 10:46:32 +0100

    tweaks

--- wrappers/parent_wrapper.py
+++ wrappers/parent_wrapper.py
@@ -3,22 +3,24 @@ import random
 import pyRDDLGym
 import numpy as np
 import gymnasium as gym
-from utils import to_graphviz
+from .utils import to_graphviz, to_graphviz_alt
 
 
 class RDDLGraphWrapper(gym.Wrapper):
     metadata = {"render.modes": ["human"]}
 
     def __init__(self, domain: str, instance: int, render_mode: str = "human") -> None:
-        env = pyRDDLGym.make(domain, instance, enforce_action_constraints=True)  # type: ignore
+        env: gym.Env = pyRDDLGym.make(domain, instance, enforce_action_constraints=True)  # type: ignore
         model = env.model  # type: ignore
         object_to_type: dict[str, str] = copy(model.object_to_type)  # type: ignore
         types = set(object_to_type.values())  # type: ignore
+        type_list = sorted(types)
         num_objects = sum(env.model.object_counts(types))  # type: ignore
         state_fluents: list[str] = list(env.model.state_fluents.keys())  # type: ignore
-        action_groundings: list[str] = list(
+        action_groundings: set[str] = set(
             dict(env.model.ground_vars_with_values(model.action_fluents)).keys()  # type: ignore
         )
+        action_groundings.add("noop")
         groundings: list[str] = [
             g
             for _, v in env.model.variable_groundings.items()  # type: ignore
@@ -27,7 +29,10 @@ class RDDLGraphWrapper(gym.Wrapper):
         ]
 
         groundings = sorted(set(groundings))
+        groundings = ["noop"] + groundings
+
         variable_params: dict[str, list[str]] = copy(env.model.variable_params)  # type: ignore
+        variable_params["noop"] = []
         type_to_fluent: dict[str, list[str]] = {
             value[0]: [k for k, v in variable_params.items() if v == value]
             for _, value in variable_params.items()
@@ -43,68 +48,58 @@ class RDDLGraphWrapper(gym.Wrapper):
         assert max(arities.values()) <= 2, "Only up to binary predicates are supported"
         non_fluents: list[str] = list(env.model.non_fluents.keys())  # type: ignore
 
-        action_values = {k: np.bool_(True) for k in action_groundings}
+        action_values = {k: np.bool_(False) for k in action_groundings}
 
         non_fluent_values: dict[str, int] = dict(
             env.model.ground_vars_with_values(model.non_fluents)  # type: ignore
         )
+        action_fluents: list[str] = list(model.action_fluents.keys())  # type: ignore
+
+        relations = non_fluents + state_fluents + action_fluents + ["noop"]
+
+        relation_list = sorted(relations)
 
         object_terms: list[str] = list(model.object_to_index.keys())  # type: ignore
-        action_fluents: list[str] = list(model.action_fluents.keys())  # type: ignore
-        symbol_list = sorted(
-            object_terms + non_fluents + state_fluents + action_fluents
-        )
+        object_list = sorted(object_terms)
+        obj_to_idx = {symb: idx for idx, symb in enumerate(object_list)}
+        # symbol_list = sorted(
+        #     object_terms + non_fluents + state_fluents + action_fluents + ["noop"]
+        # )
         variable_ranges: dict[str, str] = model._variable_ranges  # type: ignore
-
-        self.variable_ranges = variable_ranges  # type: ignore
-        symb_to_idx = {symb: idx for idx, symb in enumerate(symbol_list)}
+        variable_ranges["noop"] = "bool"
+        # symb_to_idx = {symb: idx for idx, symb in enumerate(symbol_list)}
+        num_relations = len(relation_list)
+        rel_to_idx = {symb: idx for idx, symb in enumerate(relation_list)}
+        num_edges = sum(arities.values())
+        num_groundings = len(groundings)
+        self.variable_ranges: dict[str, str] = variable_ranges
         self.non_fluents_values = non_fluent_values
-
         self.instance = instance
         self.domain = domain
         self.type_to_fluent = type_to_fluent
         self.model = model
+        self.action_groundings = action_groundings
+        self.num_types = len(type_list)
+        self.num_relations = num_relations
         self.num_objects = num_objects
         self.arities = arities
         self.arities_to_fluent = arities_to_fluent
         self.non_fluents = non_fluents
         self.action_values = action_values
-        self.object_to_type = object_to_type
+        self.obj_to_type: dict[str, str] = object_to_type
         self.groundings = groundings
         self.state_fluents = state_fluents
-        self.idx_to_symb = symbol_list
-        self.symb_to_idx = symb_to_idx
-        self.env = env
+        self.idx_to_obj = object_list
+        self.obj_to_idx = obj_to_idx
+        self.idx_to_rel = relation_list
+        self.rel_to_idx = rel_to_idx
+        self.type_to_idx = {symb: idx for idx, symb in enumerate(type_list)}
+        self.idx_to_type = type_list
+
+        self.env: gym.Env[gym.spaces.Dict, gym.spaces.Discrete] = env
         self.iter = 0
-        self.observation_space = gym.spaces.Dict(
-            {
-                "nodes": gym.spaces.Box(
-                    low=0, high=1, shape=(len(groundings), 2), dtype=np.float32
-                ),
-                "object_nodes": gym.spaces.Box(
-                    low=0, high=len(symbol_list), shape=(num_objects,), dtype=np.int32
-                ),
-                "edge_indices": gym.spaces.Box(
-                    low=0,
-                    high=num_objects,
-                    shape=(len(action_groundings) + len(non_fluent_values), 2),
-                    dtype=np.uint,
-                ),
-                "edge_attributes": gym.spaces.Box(
-                    low=0,
-                    high=1,
-                    shape=(len(action_groundings) + len(non_fluent_values),),
-                    dtype=np.uint,
-                ),
-                "numeric": gym.spaces.Box(
-                    low=0,
-                    high=1,
-                    shape=(len(symbol_list),),
-                    dtype=np.bool_,  # type: ignore
-                ),
-            }
-        )
-        # self.action_space = gym.spaces.Discrete(len(groundings))
+
+        self.action_space = gym.spaces.Discrete(num_groundings)
 
     def render(self):
         obs = self.last_obs
@@ -113,18 +108,18 @@ class RDDLGraphWrapper(gym.Wrapper):
         object_nodes = obs["object"]
         edge_indices = obs["edge_index"]
         edge_attributes = obs["edge_attr"]
-        numeric = obs["numeric"]
+        # numeric = obs["numeric"]
 
         with open(f"{self.domain}_{self.instance}_{self.iter}.dot", "w") as f:
             f.write(
-                to_graphviz(
+                to_graphviz_alt(
                     nodes_classes,
                     node_values,
                     object_nodes,
                     edge_indices,
                     edge_attributes,
-                    self.idx_to_symb,
-                    numeric,
+                    self.idx_to_type,
+                    self.idx_to_rel,
                 )
             )
 

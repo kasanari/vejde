4c87a206696a6886f72cb89fcafac1ffa7c5e050
Jakob Nyberg <jaknyb@kth.se>
2024-12-17 09:54:33 +0100

    update

--- test/test_trained_agent.py
+++ test/test_trained_agent.py
@@ -3,23 +3,12 @@ import torch
 from test_imitation_mdp import evaluate, save_eval_data
 
 from gnn import Config, GraphAgent
-from wrappers.wrapper import register_env
-
-
-def load_agent(path: str) -> tuple[GraphAgent, Config]:
-    data = torch.load(path, weights_only=False)
-    config = Config(**data["config"])
-    agent = GraphAgent(config)
-    agent.load_state_dict(
-        {k.replace("agent.", ""): v for k, v in data["state_dict"].items()}
-    )
-
-    return agent, config
+from rddl import register_env
 
 
 def test_agent():
-    path = "conditional_bandit.rddl__ppo_gnn__1.pth"
-    agent, config = load_agent(path)
+    path = "conditional_bandit.rddl__ppo_gnn__0.pth"
+    agent, config = GraphAgent.load_agent(path)
     domain = "rddl/conditional_bandit.rddl"
     instance = "rddl/conditional_bandit_i0.rddl"
     env: gym.Env = gym.make(
@@ -28,9 +17,8 @@ def test_agent():
         instance=instance,
         # add_inverse_relations=False,
         # types_instead_of_objects=False,
-        render_mode="idx",
     )
-    data = [evaluate(env, agent, i) for i in range(3)]
+    data = [evaluate(env, agent, 0) for i in range(3)]
     save_eval_data(data)
 
 

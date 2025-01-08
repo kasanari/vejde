fecc662195acb0b51ae3db41535af88ab733dd34
Jakob Nyberg <jaknyb@kth.se>
2025-01-08 13:32:43 +0100

    add test for rnn agent

--- test/test_trained_agent.py
+++ test/test_trained_agent.py
@@ -2,12 +2,36 @@ import gymnasium as gym
 import torch
 from test_imitation_mdp import evaluate, save_eval_data
 
-from gnn import Config, GraphAgent
-from rddl import register_env
+from gnn import Config, GraphAgent, RecurrentGraphAgent
+
 import logging
 
 
+def test_rnn_agent():
+    from rddl import register_pomdp_env as register_env
+
+    path = "blink_enough_bandit.pth"
+    agent, config = RecurrentGraphAgent.load_agent(path)
+    domain = "rddl/blink_enough_bandit.rddl"
+    instance = "rddl/blink_enough_bandit_i0.rddl"
+    env: gym.Env = gym.make(
+        register_env(),
+        domain=domain,
+        instance=instance,
+        # add_inverse_relations=False,
+        # types_instead_of_objects=False,
+    )
+    torch.set_printoptions(precision=2, sci_mode=False)
+    logfile = open("rnn_test.log", "w")
+    logging.basicConfig(level=logging.DEBUG, format="%(message)s", stream=logfile)
+    data = [evaluate(env, agent, 0) for i in range(1)]
+    save_eval_data(data)
+    logfile.close()
+
+
 def test_agent():
+    from rddl import register_env
+
     path = "conditional_bandit.pth"
     agent, config = GraphAgent.load_agent(path)
     domain = "rddl/conditional_bandit.rddl"
@@ -28,4 +52,4 @@ def test_agent():
 
 
 if __name__ == "__main__":
-    test_agent()
+    test_rnn_agent()

ba1ec8dc7cc9ccea0582e64ad834091fa69aecb6
Jakob Nyberg <jaknyb@kth.se>
2025-01-26 14:58:42 +0100

    add option for layer norm in mlp

--- src/regawa/gnn/gnn_classes.py
+++ src/regawa/gnn/gnn_classes.py
@@ -1,6 +1,6 @@
 import torch.nn.init as init
 from torch import Tensor
-from torch.nn import Embedding, Linear, Module, LayerNorm
+from torch.nn import Embedding, Linear, Module, LayerNorm, Sequential
 
 
 class EmbeddingLayer(Module):
@@ -15,26 +15,40 @@ class EmbeddingLayer(Module):
         # self.bias = Parameter(torch.zeros(num_embeddings, embedding_dim))
         # self.activation = activation
 
+        self.transform = Sequential(
+            self.embedding,
+            self.layer_norm,
+        )
+
     def forward(self, x: Tensor) -> Tensor:
-        # bias = self.bias[x]
-        x = self.embedding(x)
-        x = self.layer_norm(x)
-        return x
+        return self.transform(x)
 
 
 class MLPLayer(Module):
-    def __init__(self, in_features: int, out_features: int, activation: Module):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        activation: Module,
+        use_layer_norm: bool = False,
+    ):
         super().__init__()  # type: ignore
         linear = Linear(in_features, out_features)
         init.orthogonal_(linear.weight)  # type: ignore
         init.constant_(linear.bias, 0.0)
-        self.linear = linear
-        self.activation = activation
+
+        layer_norm = LayerNorm(out_features, elementwise_affine=False)
+
+        layers = (
+            (linear, layer_norm, activation) if use_layer_norm else (linear, activation)
+        )
+
+        self.transform = Sequential(
+            *layers,
+        )
 
     def forward(self, x: Tensor) -> Tensor:
-        x = self.linear(x)
-        x = self.activation(x)
-        return x
+        return self.transform(x)
 
     def __str__(self) -> str:
-        return f"Weight:\n{self.linear.weight}\nBias:\n{self.linear.bias}"
+        return f"Weight:\n{self.transform[0].weight}\nBias:\n{self.transform[0].bias}"

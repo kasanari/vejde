c9f4ac25ab1d9cb540f32cbfeec58b9a5f8a828e
Jakob Nyberg <jaknyb@kth.se>
2025-01-29 11:11:07 +0100

    rddl shuffle instance env

--- src/regawa/rddl/__init__.py
+++ src/regawa/rddl/__init__.py
@@ -12,6 +12,97 @@ from .rddl_model import RDDLModel
 from .rddl_add_non_fluents_wrapper import RDDLAddNonFluents
 from .rddl_convert_enums_wrapper import RDDLConvertEnums
 
+from rddlrepository import RDDLRepoManager
+import numpy as np
+
+
+from pyRDDLGym.core.parser.parser import RDDLParser
+from pyRDDLGym.core.parser.reader import RDDLReader
+from pyRDDLGym.core.compiler.model import RDDLLiftedModel
+
+
+def make_env(
+    domain, instance, model, enforce_action_constraints=False, has_enums=False
+):
+    env = pyRDDLGym.make(
+        domain,
+        instance,
+        enforce_action_constraints=enforce_action_constraints,
+    )  # type: ignore
+    env = RDDLAddNonFluents(env)
+    env = RDDLToTuple(env)
+    if has_enums:
+        env = RDDLConvertEnums(env)
+    env: gymnasium.Env[Dict, MultiDiscrete] = GroundedGraphWrapper(env, model=model)
+    return env
+
+
+class RDDLShuffleInstancesEnv(gymnasium.Env[Dict, MultiDiscrete]):
+    def __init__(
+        self,
+        domain: str,
+        instances: list[str],
+        seed: int,
+        enforce_action_constraints: bool = False,
+    ) -> None:
+        super().__init__()
+
+        manager = RDDLRepoManager()
+        problem = manager.get_problem(domain)
+
+        reader = RDDLReader(problem.get_domain(), problem.get_instance(instances[0]))
+        parser = RDDLParser(lexer=None, verbose=False)
+        parser.build()
+        rddl = parser.parse(reader.rddltxt)
+        model = RDDLModel(RDDLLiftedModel(rddl))
+
+        # define the RDDL model
+
+        self.model = model
+        self.domain = domain
+        self.rng = np.random.default_rng(seed)
+        self.instances = instances
+        self.enforce_action_constraints = enforce_action_constraints
+        self.has_enums = len(self.model.model.enum_types) > 0
+
+        # just to get the action and observation spaces
+        self.env = make_env(
+            domain, instances[0], model, enforce_action_constraints, self.has_enums
+        )
+
+    @property
+    def action_space(self):
+        return self.env.action_space
+
+    @property
+    def observation_space(self):
+        return self.env.observation_space
+
+    def step(
+        self, action: MultiDiscrete
+    ) -> tuple[Dict, SupportsFloat, bool, bool, dict[str, Any]]:
+        return self.env.step(action)
+
+    def reset(
+        self, *, seed: int | None = None, options: dict[str, Any] | None = None
+    ) -> tuple[Dict, dict[str, Any]]:
+        super().reset(seed=seed)
+
+        instance = self.rng.choice(self.instances)
+
+        self.env = make_env(
+            self.domain,
+            instance,
+            self.model,
+            self.enforce_action_constraints,
+            self.has_enums,
+        )
+
+        return self.env.reset(seed=seed, options=options)
+
+    def render(self) -> str:
+        return self.env.render()
+
 
 class RDDLGraphEnv(gymnasium.Env[Dict, MultiDiscrete]):
     def __init__(
@@ -90,6 +181,15 @@ def register_env():
     gymnasium.register(
         id=env_id,
         entry_point="regawa.rddl:RDDLGraphEnv",
+    )
+    return env_id
+
+
+def register_shuffle_env():
+    env_id = "RDDLShuffleInstancesEnv-v0"
+    gymnasium.register(
+        id=env_id,
+        entry_point="regawa.rddl:RDDLShuffleInstancesEnv",
     )
     return env_id
 

748b68a39346506c88a7de0d2ec244b915be8748
Jakob Nyberg <jaknyb@kth.se>
2025-02-06 10:54:04 +0100

    add removefalse to exports

--- src/regawa/wrappers/__init__.py
+++ src/regawa/wrappers/__init__.py
@@ -3,6 +3,7 @@ from .pomdp_wrapper import StackingGroundedGraphWrapper
 from .index_wrapper import IndexActionWrapper
 from .stacking_wrapper import StackingWrapper
 from .add_constants_wrapper import AddConstantsWrapper
+from .remove_false_wrapper import RemoveFalseWrapper
 
 __all__ = [
     "GroundedGraphWrapper",
@@ -10,4 +11,5 @@ __all__ = [
     "IndexActionWrapper",
     "StackingWrapper",
     "AddConstantsWrapper",
+    "RemoveFalseWrapper",
 ]

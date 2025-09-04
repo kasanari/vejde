c608acecb7a81f110cd97e6ba3d7dd925c4c742e
Jakob Nyberg <jaknyb@kth.se>
2025-09-04 08:50:44 +0200

    comments

--- src/regawa/model/utils.py
+++ src/regawa/model/utils.py
@@ -8,6 +8,9 @@ def max_arity(model: BaseModel):
 
 
 def fn_type_attributes(model: BaseModel):
+    """
+    Returns a function that takes an object type and returns a tuple of attribute names for that type
+    """
     vp = model.variable_params  # type: ignore
     d: dict[str, tuple[str, ...]] = {
         value[0]: tuple([k for k, v in vp.items() if v == value])  # type: ignore
@@ -23,6 +26,9 @@ def fn_type_attributes(model: BaseModel):
 
 
 def fn_fluents_of_arity(model: BaseModel):
+    """
+    Returns a function that takes an arity and returns a tuple of fluents with that arity.
+    """
     arities = set(model.arity(fluent) for fluent in model.fluents)  # type: ignore
 
     d: dict[int, tuple[str, ...]] = {
@@ -38,6 +44,11 @@ def fn_fluents_of_arity(model: BaseModel):
 
 
 def fn_valid_action_fluents_given_type(model: BaseModel):
+    """
+    Returns a function that takes an object type and returns a tuple of booleans indicating
+    whether each action fluent is valid for that object based on the fluent's parameter types.
+    The null object is assumed to be a valid object type for all fluents.
+    """
     @cache
     def is_valid(fluent: str, o_t: str) -> bool:
         return (
@@ -52,6 +63,12 @@ def fn_valid_action_fluents_given_type(model: BaseModel):
 
 
 def fn_valid_action_fluents_given_arity(model: BaseModel):
+    """
+    Returns a function that takes an object type and returns a tuple of booleans indicating
+    whether each action fluent is valid for that object based on the arity of the predicate.
+    Nullary predicates do not take any object parameters, so they are never valid for any object type, except the null object.
+    The null object is assumed to be a valid object type for all fluents.
+    """
     @cache
     def is_valid(fluent: str, o_t: str) -> bool:
         return (

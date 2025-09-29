8278e951a8ae70a34fd70387925c3cb6ce134b2a
Jakob Nyberg <jaknyb@kth.se>
2025-09-29 15:26:52 +0200

    remove old option in loading

--- src/regawa/policy/load.py
+++ src/regawa/policy/load.py
@@ -14,9 +14,6 @@ def load_agent(cls: type[T], path: str, device: str = "cpu") -> tuple[T, AgentCo
 
     data["config"]["hyper_params"] = GNNParams(**data["config"]["hyper_params"])
 
-    if "remove_false_fluents" not in data["config"]:
-        data["config"]["remove_false_fluents"] = False  # for backward compatibility
-
     config = AgentConfig(**data["config"])
     agent = cls(config, None)
     agent.load_state_dict(data["state_dict"])

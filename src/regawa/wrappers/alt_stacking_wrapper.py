e8624271578f60fe068c5bc5cab3979c73503193
Jakob Nyberg <jaknyb@kth.se>
2025-12-09 12:02:02 +0100

    use relative time

--- src/regawa/wrappers/alt_stacking_wrapper.py
+++ src/regawa/wrappers/alt_stacking_wrapper.py
@@ -13,7 +13,63 @@ from regawa.model.base_grounded_model import (
 
 class TimeEntry(NamedTuple):
     start_time: int
+    end_time: int
+
+
+class RelativeTimeEntry(NamedTuple):
     duration: int
+    distance_to_prev: int
+
+
+class CompressedStack(NamedTuple):
+    te: TimeEntry
+    value: GroundingRange
+
+
+class RelativeCompressedStack(NamedTuple):
+    rte: RelativeTimeEntry
+    value: GroundingRange
+
+
+def get_time_entry(
+    current_time: int,
+    current_value: GroundingRange,
+    prev_time: int,
+    prev_value: GroundingRange,
+    start_time: int,
+) -> tuple[CompressedStack, CompressedStack | None]:
+    time_skip = current_time != prev_time + 1  # check for gaps in the sequence
+    value_change = current_value != prev_value  # check for changes in value
+
+    if time_skip:
+        # if there is a gap, end the previous segment and start a new one
+        return (
+            CompressedStack(
+                TimeEntry(start_time, prev_time),
+                prev_value,
+            ),  # end previous segment
+            CompressedStack(
+                TimeEntry(current_time, current_time), current_value
+            ),  # start new segment
+        )
+
+    if value_change:
+        # if the value has changed, end the previous segment and start a new one
+        return (
+            CompressedStack(
+                TimeEntry(start_time, current_time), prev_value
+            ),  # end previous segment
+            CompressedStack(
+                TimeEntry(current_time, current_time),
+                current_value,
+            ),  # start new segment
+        )
+
+    # if the value has not changed, or there are any gaps, continue current segment
+    return CompressedStack(
+        TimeEntry(start_time, current_time),
+        current_value,
+    ), None
 
 
 def compress_stack(o: Sequence[tuple[int, GroundingRange]]):
@@ -27,29 +83,58 @@ def compress_stack(o: Sequence[tuple[int, GroundingRange]]):
 
     first_entry = o[0]
 
-    start_time, prev_time, prev_value = first_entry[0], first_entry[0], first_entry[1]
+    segment = CompressedStack(
+        TimeEntry(first_entry[0], first_entry[0]),
+        first_entry[1],
+    )
 
     for time, value in o[1:]:
-        time_skip = time != prev_time + 1  # check for gaps in the sequence
-        value_skip = value != prev_value  # check for changes in value
+        segment, new_segment = get_time_entry(
+            time,
+            value,
+            segment.te.end_time,
+            segment.value,
+            segment.te.start_time,
+        )
 
-        if time_skip:
-            compressed_stack.append(
-                (TimeEntry(start_time, prev_time), prev_value)
-            )  # add the previous segment
-            start_time = time  # start a new segment
-            prev_value = value
+        if new_segment is not None:
+            compressed_stack.append(segment)
+            segment = new_segment
 
-        if value_skip:
-            compressed_stack.append((TimeEntry(start_time, time), prev_value))
-            start_time = time  # start a new segment
-            prev_value = value
+    compressed_stack.append(segment)
 
-        prev_time = time
+    return tuple(compressed_stack)
 
-    compressed_stack.append((TimeEntry(start_time, prev_time), prev_value))
 
-    return compressed_stack
+def to_relative_time_entry(te: TimeEntry, prev_te: TimeEntry) -> RelativeTimeEntry:
+    duration = te.end_time - te.start_time + 1
+    distance_to_prev = max(te.start_time - prev_te.end_time, 0)
+    return RelativeTimeEntry(duration, distance_to_prev)
+
+
+def stack_to_relative_time(
+    compressed_stack: Sequence[CompressedStack],
+) -> list[RelativeCompressedStack]:
+    relative_stack: list[RelativeCompressedStack] = []
+
+    first_entry = compressed_stack[0]
+    prev_te = first_entry.te
+
+    for cs in compressed_stack:
+        rte = to_relative_time_entry(cs.te, prev_te)
+        relative_stack.append(RelativeCompressedStack(rte, cs.value))
+        prev_te = cs.te
+
+    return relative_stack
+
+
+def obs_to_relative_time(
+    obs: StackedGroundObs,
+):
+    return {
+        grounding: stack_to_relative_time(compressed_stack)
+        for grounding, compressed_stack in obs.items()
+    }
 
 
 def compress_stacked_obs(
@@ -74,16 +159,17 @@ def compress_stacked_obs(
 
 def create_obs(obs: TemporalGroundObs) -> StackedGroundObs:
     sorted_obs = sorted(obs.items(), key=lambda x: x[0][1])
-    grouped_obs = groupby(sorted_obs, key=lambda x: x[0][1])
 
     new_obs: StackedGroundObs = {
         grounding: tuple([(time, value) for (time, _), value in values])
-        for grounding, values in grouped_obs
+        for grounding, values in groupby(sorted_obs, key=lambda x: x[0][1])
     }
 
     new_obs = compress_stacked_obs(new_obs)
 
-    return new_obs
+    relative = obs_to_relative_time(new_obs)
+
+    return relative
 
 
 class StackingWrapper(gym.Wrapper[StackedGroundObs, GroundObs, GroundObs, GroundObs]):

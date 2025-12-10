a904e75cc14c984a7286f296064d6df03dd9b62d
Jakob Nyberg <jaknyb@kth.se>
2025-12-10 14:20:29 +0100

    update q agent

--- src/regawa/policy/q_agent/gnn_q_agent.py
+++ src/regawa/policy/q_agent/gnn_q_agent.py
@@ -1,7 +1,7 @@
 # type: ignore
 import torch.nn as nn
 
-from regawa.embedding.boolean import NegativeBiasBooleanEmbedder
+from regawa.embedding.boolean import PositiveNegativeBooleanEmbedder
 from regawa.embedding.numeric import NumericEmbedder
 from regawa.policy import ActionMode, AgentConfig
 from .q_action_then_node import QActionThenNode
@@ -11,69 +11,80 @@ from regawa.data import TorchFactorGraph, HeteroBatchData
 from regawa.gnn import BipartiteGNN
 from regawa.embedding import (
     EmbeddingLayer,
-    merge_graphs,
 )
-
+from torch import Generator as Rngs
+from regawa.embedding import (
+    fn_embed_graph,
+    fn_embed_heterobatch,
+)
 
 class GraphQAgent(nn.Module):
     def __init__(
         self,
         config: AgentConfig,
+        rngs: Rngs,
+        device: str = "cpu",
     ):
         super().__init__()  # type: ignore
 
+        hyper_params = config.hyper_params
+        embed_dim = config.hyper_params.embedding_dim
+        factor_embedding = EmbeddingLayer(
+            config.num_object_classes, embed_dim, rngs
+        )
+
+        predicate_embedding = EmbeddingLayer(
+            config.num_predicate_classes, embed_dim, rngs
+        )
+
+        boolean_embedder = PositiveNegativeBooleanEmbedder(
+            embed_dim,
+            predicate_embedding,
+            rngs,
+        )
+
+        numeric_embedder = NumericEmbedder(
+            embed_dim,
+            hyper_params.activation,
+            predicate_embedding,
+        )
+
+        edge_attr_embedding = EmbeddingLayer(
+            config.arity, embed_dim, rngs, use_padding=False
+        )
+
+        self.message_pass = BipartiteGNN(
+            hyper_params.layers,
+            embed_dim,
+            hyper_params.aggregation,
+            hyper_params.activation,
+            rngs,
+        )
+        self.embed_heterobatch = fn_embed_heterobatch(
+            fn_embed_graph(
+                boolean_embedder,
+                factor_embedding,
+                edge_attr_embedding,
+            ),
+            fn_embed_graph(
+                numeric_embedder,
+                factor_embedding,
+                edge_attr_embedding,
+            ),
+        )
         self.config = config
-
-        self.factor_embedding = EmbeddingLayer(
-            config.num_object_classes, config.embedding_dim
-        )
-
-        self.predicate_embedding = EmbeddingLayer(
-            config.num_predicate_classes, config.embedding_dim
-        )
-
-        self.boolean_embedder = NegativeBiasBooleanEmbedder(
-            config.embedding_dim,
-            self.predicate_embedding,
-        )
-
-        self.numeric_embedder = NumericEmbedder(
-            config.embedding_dim,
-            config.activation,
-            self.predicate_embedding,
-        )
-
-        self.p_gnn = BipartiteGNN(
-            config.layers,
-            config.embedding_dim,
-            config.aggregation,
-            config.activation,
-        )
+        self._device = device
 
         qfunc = (
             QActionThenNode
-            if config.action_mode == ActionMode.ACTION_THEN_NODE
+            if hyper_params.action_mode == ActionMode.ACTION_THEN_NODE
             else QNodeThenAction
         )
-        self.qfunc = qfunc(config.num_actions, config.embedding_dim)
+        self.qfunc = qfunc(config.num_actions, embed_dim)
+
 
     def embed(self, data: HeteroBatchData) -> TorchFactorGraph:
-        return self.p_gnn(
-            merge_graphs(
-                embed(
-                    data.boolean,
-                    self.boolean_embedder,
-                    self.factor_embedding,
-                    self.boolean_embedder,
-                ),
-                embed(
-                    data.numeric,
-                    self.numeric_embedder,
-                    self.factor_embedding,
-                    self.numeric_embedder,
-                ),
-            )
-        )
+        return self.message_pass(self.embed_heterobatch(data))
 
     def forward(self, data: HeteroBatchData):
         return self.qfunc.forward(self.embed(data).factors)

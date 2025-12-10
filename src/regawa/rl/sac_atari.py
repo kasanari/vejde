cd942271f586c0ddd50fc40dcd6edcb7658b72a9
Jakob Nyberg <jaknyb@kth.se>
2025-12-10 09:43:59 +0100

    change make env

--- src/regawa/rl/sac_atari.py
+++ src/regawa/rl/sac_atari.py
@@ -77,26 +77,14 @@ class Args:
     """coefficient for scaling the autotune entropy target"""
 
 
-def make_env(env_id, seed, idx, capture_video, run_name):
-    def thunk():
-        if capture_video and idx == 0:
-            env = gym.make(env_id, render_mode="rgb_array")
-            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
-        else:
-            env = gym.make(env_id)
+def make_env(
+    env_id: str,
+):
+    def thunk() -> gym.Env[Dict, MultiDiscrete]:
+        env: gym.Env[Dict, MultiDiscrete] = gym.make(  # type: ignore
+            env_id,
+        )
         env = gym.wrappers.RecordEpisodeStatistics(env)
-
-        env = NoopResetEnv(env, noop_max=30)
-        env = MaxAndSkipEnv(env, skip=4)
-        env = EpisodicLifeEnv(env)
-        if "FIRE" in env.unwrapped.get_action_meanings():
-            env = FireResetEnv(env)
-        env = ClipRewardEnv(env)
-        env = gym.wrappers.ResizeObservation(env, (84, 84))
-        env = gym.wrappers.GrayscaleObservation(env)
-        env = gym.wrappers.FrameStackObservation(env, 4)
-
-        env.action_space.seed(seed)
         return env
 
     return thunk
